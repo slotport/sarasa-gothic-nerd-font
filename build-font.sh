2d1d2a7e9423835ad528c289827ec031178c8dac
noname <noaddress@example.com>
2022-02-20 15:10:23 +0900

    Squashed commit of the following:
    commit 9be7be4f73052e8803940442694dec642984c42f
    Author: noname <noaddress@example.com>
    Date:   Sun Feb 20 15:00:33 2022 +0900
    
        cicd:
    
    commit 7122104bf6fff9616617ba28e58d0a0f419ff9b7
    Author: noname <noaddress@example.com>
    Date:   Sun Feb 20 14:50:25 2022 +0900
    
        cicd:
    
    commit 90af8edf0fb96561f8a300b58c1d22535623bedd
    Author: noname <noaddress@example.com>
    Date:   Sun Feb 20 14:41:08 2022 +0900
    
        cicd:
    
    commit d4ffbd4acea278a5f896508d4c4c32fbed020bf5
    Author: noname <noaddress@example.com>
    Date:   Sun Feb 20 14:20:47 2022 +0900
    
        cicd:
    
    commit a20b8cf44acdcb545595f48485eb9dd139912fab
    Author: noname <noaddress@example.com>
    Date:   Sun Feb 20 14:19:50 2022 +0900
    
        cicd:
    
    commit d717845c659358ba3abcd629e428b087fb698dd7
    Author: noname <noaddress@example.com>
    Date:   Sun Feb 20 14:09:37 2022 +0900
    
        cicd:
    
    commit c693cd8dbc51d83a48d2b4a0c25ee8a84755b443
    Author: noname <noaddress@example.com>
    Date:   Sun Feb 20 14:04:12 2022 +0900
    
        cicd:
    
    commit a40786ad4618d3d4a7305b7f66fed8c33a5c6d42
    Author: noname <noaddress@example.com>
    Date:   Sun Feb 20 14:01:55 2022 +0900
    
        cicd:
    
    commit db70966376be60778b171cad7fbb595fdfa8996f
    Author: noname <noaddress@example.com>
    Date:   Sun Feb 20 13:58:48 2022 +0900
    
        cicd:

--- build-font.sh
+++ build-font.sh
@@ -3,11 +3,14 @@
 set -u
 set -e
 
-SARASA_GOTHIC_URL=https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.32.10/sarasa-gothic-ttf-0.32.10.7z
+SARASA_GOTHIC_URL=https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.35.9/sarasa-gothic-ttf-0.35.9.7z
 SARASA_GOTHIC_FILE=${SARASA_GOTHIC_URL##*/}
 FONTFORGE_VERSION_MINIMAL=20190801
 UNITETTC_URL=http://yozvox.web.fc2.com/unitettc.zip
 UNITETTC_FILE=${UNITETTC_URL##*/}
+if [[ -z ${UNITETTC:-} ]]; then
+    UNITETTC=unitettc64
+fi
 
 # ================================================================
 # precondition check
@@ -24,7 +27,7 @@ if type fontforge >/dev/null 2>&1; then
     :
 else
     echo "missing: fontforge"
-    echo "install ffontforge"
+    echo "install fontforge"
     exit 1
 fi
 
@@ -42,7 +45,7 @@ else
     exit 1
 fi
 
-if type unitettc64 >/dev/null 2>&1; then
+if type ${UNITETTC} >/dev/null 2>&1; then
     :
 else
     echo "missing: unitettc64"
@@ -53,49 +56,71 @@ fi
 
 # ================================================================
 
-if [[ ! -d nerd-fonts ]]; then
-    git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git -b v2.1.0
+if [[ ! -d nerd-fonts-master ]]; then
+    wget -q https://github.com/ryanoasis/nerd-fonts/archive/refs/heads/master.tar.gz -O - | tar xzf -
+    #git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git -b v2.1.0
 fi
 
 if [[ ! -f ${SARASA_GOTHIC_FILE} ]]; then
-    wget ${SARASA_GOTHIC_URL}
+    wget -q ${SARASA_GOTHIC_URL}
 fi
 
+# unzip sarasa
 if [[ ! -d sarasa ]]; then
     7za x -osarasa ${SARASA_GOTHIC_FILE} sarasa-fixed-j-regular.ttf sarasa-fixed-j-bold.ttf
 fi
 
-cd nerd-fonts
+# apply patches to nerd font
+
+
+mkdir -p phase1_patched
+mkdir -p phase2_adjusted
 
 FONTFILE_REGULAR='Sarasa Fixed J Nerd Font Complete.ttf'
 FONTFILE_BOLD='Sarasa Fixed J Bold Nerd Font Complete.ttf'
 
-if [[ ! -f ${FONTFILE_REGULAR} ]]; then
-    fontforge -script font-patcher \
+if [[ ! -f phase1_patched/${FONTFILE_REGULAR} ]]; then
+    cd nerd-fonts-master
+    ls -l
+    fontforge -script ./font-patcher \
               ../sarasa/sarasa-fixed-j-regular.ttf \
               --quiet \
               --careful \
               --complete
+    mv "${FONTFILE_REGULAR}" "../phase1_patched/${FONTFILE_REGULAR}"
+    cd ..
 else
-    echo "already exist: '${FONTFILE_REGULAR}'"
+    echo "already exist: 'phase1_patched/${FONTFILE_REGULAR}'"
 fi
 
-if [[ ! -f ${FONTFILE_BOLD} ]]; then
-    fontforge -script font-patcher \
+if [[ ! -f phase1_patched/${FONTFILE_BOLD} ]]; then
+    cd nerd-fonts-master
+    ls -l
+    fontforge -script ./font-patcher \
               ../sarasa/sarasa-fixed-j-bold.ttf \
               --quiet \
               --careful \
               --complete
+    mv "${FONTFILE_BOLD}" "../phase1_patched/${FONTFILE_BOLD}"
+    cd ..
 else
-    echo "already exist: '${FONTFILE_BOLD}'"
+    echo "already exist: 'phase1_patched/${FONTFILE_BOLD}'"
 fi
 
-ttx -ft 'OS/2' "${FONTFILE_REGULAR}"
+cd phase2_adjusted
+ttx -ft 'OS/2' "../phase1_patched/${FONTFILE_REGULAR}"
+mv "../phase1_patched/${FONTFILE_REGULAR%.ttf}.ttx" .
 sed -i 's@<xAvgCharWidth value=".*"/>@<xAvgCharWidth value="500"/>@' "${FONTFILE_REGULAR%.ttf}.ttx"
+cp "../phase1_patched/${FONTFILE_REGULAR}" .
 ttx -fm "${FONTFILE_REGULAR}" "${FONTFILE_REGULAR%.ttf}.ttx"
 
-ttx -ft 'OS/2' "${FONTFILE_BOLD}"
+ttx -ft 'OS/2' "../phase1_patched/${FONTFILE_BOLD}"
+mv "../phase1_patched/${FONTFILE_BOLD%.ttf}.ttx" .
 sed -i 's@<xAvgCharWidth value=".*"/>@<xAvgCharWidth value="500"/>@' "${FONTFILE_BOLD%.ttf}.ttx"
+cp "../phase1_patched/${FONTFILE_BOLD}" .
 ttx -fm "${FONTFILE_BOLD}" "${FONTFILE_BOLD%.ttf}.ttx"
+cd ..
 
-unitettc64 ../SarasaFixedJNerdFont.ttc "${FONTFILE_REGULAR}" "${FONTFILE_BOLD}"
+${UNITETTC} ./SarasaFixedJNerdFont.ttc "phase2_adjusted/${FONTFILE_REGULAR}" "phase2_adjusted/${FONTFILE_BOLD}"
+
+echo generated ./SarasaFixedJNerdFont.ttc
